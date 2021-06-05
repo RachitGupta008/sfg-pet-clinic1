2fb24619f6eaec8ae589f0314a88664c0771b84d
Rachit <rachit.gupta97@yahoo.com>
2021-06-05 17:09:01 +0530

    entity to JPA. Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Speciality.java
@@ -1,6 +1,13 @@
 package guru.springframework.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends  BaseEntity {
+    @Column(name = "description")
     private String desciption;
 
     public String getDesciption() {
