149a5be041eda8cf2e8a011cdf42183acd802d79
Rachit <rachit.gupta97@yahoo.com>
2021-06-05 18:44:28 +0530

    pet  service jpa implementation. Closes #43

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/springdatajpa/PetDataJpaService.java
@@ -0,0 +1,50 @@
+package guru.springframework.petclinic.services.springdatajpa;
+
+import guru.springframework.petclinic.model.Pet;
+import guru.springframework.petclinic.repositories.PetRepository;
+import guru.springframework.petclinic.repositories.PetTypeRepository;
+import guru.springframework.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetDataJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetDataJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet findByID(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet type) {
+        return petRepository.save(type);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+         petRepository.findAll().forEach(pets::add);
+         return pets;
+
+    }
+
+    @Override
+    public void delete(Pet type) {
+        petRepository.delete(type);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
